230569834add30be0770d48149cfc2972e2097ba
mahsundemir1903 <36710826+mahsundemir1903@users.noreply.github.com>
2018-02-23 02:23:45 +0300

    Update karekökalma.py

--- karekökalma.py
+++ karekökalma.py
@@ -1,12 +1,5 @@
 while 1:
     a=input("Sayı giriniz:")
     print(int(a)**(1/2))
-<<<<<<< HEAD
-    print("aaa")
-=======
-    print("bbba")
     print("kaan")
-
-    asasas
-adaa
->>>>>>> 67ab9687a156aa4ec8d93266a20eded6d2c5db12
+    print("baki")
